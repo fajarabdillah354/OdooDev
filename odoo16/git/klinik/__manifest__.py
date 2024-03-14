de543ff781105d803213028bae72d69f6ead9df4
unknown <fajardillah25@gmail.com>
2024-03-14 23:13:57 +0700

    Update

---
+++ odoo16/git/klinik/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name':'Hospital',
+    'summary':"""Odoo 16 Tutorials""",
+    'description':"""Odoo 16 Development 
+    """,
+    'license':'OPL-1',
+    'author':'Fajar Abdillah Ahmad',
+    'website':'www.odoo.com',
+    'category':'Tutorials',
+    'depends':['base'],
+    'data':[
+    ],
+    'demo':[],
+    'application':True,
+}
