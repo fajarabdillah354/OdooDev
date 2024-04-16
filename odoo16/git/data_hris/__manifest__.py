aa5de96bbb5222240c01186264efdc33f68418e6
unknown <fajardillah25@gmail.com>
2024-04-16 09:44:27 +0700

    Upload

---
+++ odoo16/git/data_hris/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name':'',
+    'summary':"""Employee """,
+    'description':""" Description Employee 
+    """,
+    'license':'OPL-1',
+    'author':'sudoerp',
+    'website':'www.odoo.com',
+    'category':'',
+    'depends':['hr'],
+    'data':[
+        # 'data/generate_department.xml',
+        # 'data/generate_contract.xml',
+        # 'data/generate_job.xml',
+        # 'data/generate_position.xml',
+        # 'data/generate_grade.xml',
+        # 'data/generate_manager.xml',
+        # 'data/generate_coach.xml',
+        # 'data/data.xml'
+    ],
+    'demo':[
+        # 'data/hr.employee.csv'
+
+    ],
+    'application':True,
+}
