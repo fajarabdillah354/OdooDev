7b3d14463c63cfccbc1c53814fa5d403f7a27b7c
unknown <fajardillah25@gmail.com>
2024-05-03 11:02:38 +0700

    Update

--- odoo16/git/data_hris/__manifest__.py
+++ odoo16/git/data_hris/__manifest__.py
@@ -7,30 +7,28 @@
     'author':'sudoerp',
     'website':'www.odoo.com',
     'category':'',
-    'depends':['hr', 'sudo_hris_employee', 'sudo_hris_attendance'],
+    'depends':['hr', 'sudo_hris_employee', 'sudo_hris_attendance', 'sudo_hris_payroll'],
     'data':[
-        'data/generate/generate_department/generate_department.xml',
-        'data/generate/generate_position/generate_position.xml',
-        'data/generate/generate_grade/generate_grade.xml',
-        'data/generate/generate_contract/generate_employee_category.xml',
-        'data/generate/generate_job_position/generate_jobposition.xml',
-        # 'data/generate/generate_contract/generate_employee.xml',
-        # 'data/generate/generate_contract/generate_my_company.xml',
-        # 'data/generate/generate_contract/generate_company.xml',
-        # 'data/generate/generate_contract/generate_working_schedule.xml',
-        # 'data/generate/generate_contract/generate_salary_structur_type.xml',
-        # 'data/generate/generate_contract/generate_bpjs_ref3.xml',
-        # 'data/generate/generate_contract/generate_bpjs_ref2.xml',
-        # 'data/generate/generate_contract/generate_bpjs_ref1.xml',
-        # 'data/generate/generate_contract/generate_bpjs.xml',
-        # 'data/generate/generate_contract/salary_structure.xml',
-        # 'data/generate/generate_contract/generate_hr_response.xml',
-        # 'data/generate/generate_contract/generate_contract.xml',
-        'data/data.xml',
+        'data/shift_codes.xml',
+        'data/generate_res_users.xml',
+        'data/generate_resource_calendar.xml',
+        'data/generate_resource_resource.xml',
+        'data/generate_hr_contract_type.xml',
+        'data/generate_hr_department.xml',
+        'data/generate_hr_position.xml',
+        'data/generate_hr_job.xml',
+        'data/generate_hr_grade.xml',
+        'data/generate_hr_payroll_structure_type.xml',
+        'data/generate_hr_work_location.xml',
+        'data/generate_hr_employee_public.xml',
+        'data/generate_hr_employee.xml',
+        'data/working_calendar.xml',
+        'data/generate_hr_leave_type.xml'
+
+
 
     ],
     'demo':[
-        # 'data/hr.employee.csv'
     ],
     'application':True,
 }
